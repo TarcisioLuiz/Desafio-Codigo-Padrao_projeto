849fb668c7491cb67bafd198e9d49ba21ec1aae2
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2023-10-20 22:34:49 -0300

    implementação do desafio de projeto

---
+++ src/main/java/org/example/Facade.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Facade {
+
+    public void migrarCliente(String nome, String cep) {
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Facade facade = new Facade();
+        facade.migrarCliente("John Doe", "51260070");    }
+}
